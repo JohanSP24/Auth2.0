471a82277bd54cf8df8636b2ca2c6fc99753b35a
JohanSP24 <josanchezpa@gmail.com>
2024-04-11 10:58:45 -0500

    Auth2.0

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-1odmya070eviyejb.us.auth0.com"
+    clientId="v37famcyPOMlrfb0cWLEemGf9p4eYI3N"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
